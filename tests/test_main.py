272f8cef0e937977eb06656beb8ff02613165c43
davidtnfsh <davidtnfsh@gmail.com>
2020-03-24 11:20:21 +0800

    fix(npm): update minimist
    fix security issue

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ from project.question1.main import q1
 from project.question2.main import q2
 
 
-def test_q1():
+def test_q1() -> None:
     q1(path="project/question1/news.rss")
     if __debug__:
         if not Path("output.txt").exists():
@@ -15,7 +15,7 @@ def test_q1():
             raise AssertionError("description.txt not found")
 
 
-def test_q2():
+def test_q2() -> None:
     q2(path="project/question1/output.txt")
     if __debug__:
         if not Path("output2.txt").exists():
